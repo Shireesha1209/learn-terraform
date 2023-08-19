15249d1208122743b49186b9adc6140f722545f6
Shireesha <shireesha1209@gmail.com>
2023-08-19 14:05:53 +0530

    terraform roboshop

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -6,7 +6,9 @@ resource "aws_instance" "instance" {
   tags = {
     Name = var.name
   }
+
 }
+
 
 resource "aws_route53_record" "record" {
   zone_id  = var.zone_id
@@ -14,4 +16,21 @@ resource "aws_route53_record" "record" {
   type     = "A"
   ttl      = 30
   records  = [aws_instance.instance.private_ip ]
+}
+
+resource "null_resource" "ansible" {
+
+  depends_on = [
+    aws_route53_record.record
+  ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/roboshop-ansible
+git pull
+sleep 30
+ansible-playbook -i ${var.name}-dev.rdevops.online, main.yml -e ansible_user=centos
+-e ansible_password=DevOps321 -e component=${var.name}
+EOF
+  }
 }
